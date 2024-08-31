77fa9d4b6c3a810822c052e9e6a383f8a111490c
Nayeem <mahfujurrahamannayeem@gmail.com>
2024-09-01 02:33:24 +0600

    Creating the Summer Sale With Non Functional

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// Linking the Tailwind intelligence Part 
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
